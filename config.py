349eba2c6b4361a8a0cdadcd702173faefc087ee
amoashi <ashkanpj.ap22@gmail.com>
2017-05-20 18:00:11 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
+#*- coding: utf-8 -*-
 redis_db = 0 # Set your redis db number 0 - 15
-token = "SET_YOUR_TOKEN_HERE"
-sudos = []
-admins = []
-errors_chat =  #Set your errors chat(group/channel/user) id here!
-log_chat =  #Set your logs chat(group/supergroup) id here!
+token = "343136282:AAEYmJ-TAkydUuxNEX1x-IjrCcavjIvXU3I"
+sudos = [265821275]
+admins = [265821275]
+errors_chat = Error #Set your errors chat(group/channel/user) id here!
+log_chat = Error #Set your logs chat(group/supergroup) id here!
